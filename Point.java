96df81140570c96d26b4466b17cbcd5f375f65cd
Igbathscaller <Igbathscaller@gmail.com>
2020-10-29 21:50:45 -0400

    Point.java

---
+++ Point.java
@@ -0,0 +1,34 @@
+public class Point{
+    
+    private double x,y;
+
+    //construct a point give coords
+    public Point(double X, double Y){
+        x=X;
+        y=Y;
+    }
+
+    //construct a point given a point  (redundant , just provides flexibility)
+    public Point(Point p){
+        x= p.x;  //private access in the same file is allowed!
+        y= p.y;
+    }
+
+    public double getX(){
+        return x;
+    }
+    
+    public double getY(){
+        return y;
+    }  
+    
+    public double distanceTo(Point other){
+        return Math.sqrt((x-other.getX())*(x-other.getX())+(y-other.getY())+(y-other.getY()));
+    }
+
+    public boolean equals(Point other) {
+        return other.getX()==x && other.getY()==y;
+    }
+
+    
+}
